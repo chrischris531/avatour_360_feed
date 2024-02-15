1c115a5bac04dd6ca8e0d815f270473d466aac81
Charmaine <73358608+ccl19@users.noreply.github.com>
2024-02-15 18:08:23 +0000

    fixed button on screen

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { AppRegistry, Environment, View } from "react-360";
+import { AppRegistry, Environment, View, StyleSheet, Text, VrButton } from "react-360";
+
 
 import VideoModule from "VideoModule";
 
@@ -16,8 +17,41 @@ class avatour_360 extends React.Component {
   }
 
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        {/* <iframe src="button.html" style={{ position: 'absolute', width: '100%', height: '100%', border: 'none' }} /> */}
+        <View style={styles.ui}>
+          {/* <VrButton onClick={() => console.log('Button clicked!')} style={styles.button}>
+            <Text style={styles.buttonText}>Click me</Text>
+          </VrButton> */}
+        </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  ui: {
+    display: "flex",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    width: 200,
+    height: 50,
+    transform: [{ translate: [300, -200, -5] }], // Adjust the Z-axis value as needed
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  }
+});
 
 AppRegistry.registerComponent("avatour_360", () => avatour_360);
