5496ae8a56e06a7e6f6907cd55bb221b421bfcb6
Kert Laansalu <kertlaansalu@gmail.com>
2024-02-19 13:18:30 +0000

    video and robot

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ import { AppRegistry, Environment, View, StyleSheet, Text, VrButton, Model } fro
 import VideoModule from "VideoModule";
 
 class avatour_360 extends React.Component {
-  componentDidMount() {
-    this.introVideo = VideoModule.createPlayer("introVideo");
-    this.introVideo.play({
-      source: {
-        url: "https://www.youtube.com/watch?v=aJ8lLdo6x_8",
-      },
-    });
-
-    Environment.setBackgroundVideo("introVideo");
-  }
+    componentDidMount() {
+      this.introVideo = VideoModule.createPlayer("introVideo");
+      this.introVideo.play({
+        source: {
+          url: "https://video.360cities.net/storyandmorecom/01669269_Pyramids-Aerial-360-VR-Stock-video-Footage-at-Egypt-Ancient-pharaonic-video-footage-ID2-3-1920x960.mp4",
+        },
+      });
+  
+      Environment.setBackgroundVideo("introVideo");
+    }
+  
 
   render() {
     return (
       <View style={styles.container}>
  
-        {/* <iframe src="button.html" style={{ position: 'absolute', border: 'none' }} /> */}
+        {/* <iframe src="button.html" style={{ position: 'absolute', border: 'none' }} />
         <View style={styles.ui}>
 
           
-        </View>
+        </View> */}
       </View>
     );
   }
